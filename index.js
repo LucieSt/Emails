a72e8b0dd3d0a4012b4a447b222f65140c07f82d
LucieSt <strakova.91@gmail.com>
2022-10-31 21:10:56 +0100

    finished basic task

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-const renderSection = (emails, element) => {
-  element.innerHTML = emails
-    .map((email) => {
-      let iconClass = 'opened';
-      if (email.unread) {
-        iconClass = 'closed';
-      };
+console.log('funguju');
+
+const api = "https://apps.kodim.cz/daweb/trening-api/apis/emails"
+
+const showEmails = (data, variable) => {
+  console.log(variable);
+  let imageClass = 'opened'
+  if (variable === 'unread') {
+    imageClass = 'closed'
+  }
+
+  const element = document.getElementById(variable);
+
+  element.innerHTML = data.map((item) => {
 
       return `
         <div class="email">
           <div class="email__head">
-            <button class="email__icon email__icon--${iconClass}"></button>
+            <button class="email__icon email__icon--${imageClass}"></button>
             <div class="email__info">
-              <div class="email__sender">${email.sender.name}</div>
-              <div class="email__subject">${email.subject}</div>
+              <div class="email__sender">${item.sender.name}</div>
+              <div class="email__subject">${item.subject}</div>
             </div>
-            <div class="email__time">${email.time}</div>
+            <div class="email__time">${item.time}</div>
           </div>
           <div class="email__body"></div>
         </div>
-      `;
-    })
-    .join('');
-};
-
-fetch(`https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=unread`)
-  .then((response) => response.json())
-  .then((data) => renderSection(data.emails, document.getElementById('unread')));
-
-fetch(`https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=read`)
-  .then((response) => response.json())
-  .then((data) => renderSection(data.emails, document.getElementById('read')));
-
+      `
+  }).join('');
   
+}
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=unread')
+.then((response) => response.json())
+.then((data) => showEmails(data.emails, 'unread'));
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=read')
+.then((response) => response.json())
+.then((data) => showEmails(data.emails, 'read'));
